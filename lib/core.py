ee06573e7d58a45daa235bf6104ebf70d1f16e77
thibaultconte <thibault1.conte@epitech.eu>
2023-11-29 11:20:53 +0100

    feat(build): Ajout des commandes activate, deactivate, uninstall et de l'argument --version

--- lib/core.py
+++ lib/core.py
@@ -3,37 +3,51 @@ import os
 import argparse
 import shutil
 import subprocess
+from colorama import Fore, Style
 
 def get_user_input(prompt):
-    return input(prompt).strip()
+    dedent_prompt = prompt.strip().replace('\n', ' ').replace('  ', ' ')
+    joined_prompt = ' '.join(dedent_prompt.strip().split())
+    input_str = input(joined_prompt).strip()
+    reformatted_str = input_str.replace(',', ' ')
+    return reformatted_str
 
 def init_command():
+    print(f"{Fore.LIGHTBLUE_EX}{Style.BRIGHT}Lancement de l'initialisation de Hookity...{Style.RESET_ALL}")
     current_directory = os.getcwd()
     
     git_directory = os.path.join(current_directory, '.git')
     if not os.path.isdir(git_directory):
-        print("Il semble que vous ne soyez pas dans un répertoire Git.")
+        print(f"{Fore.LIGHTRED_EX}Il semble que vous ne soyez pas dans un répertoire Git.{Style.RESET_ALL}")
         return
 
     if not os.listdir(git_directory):
-        print("Le répertoire .git est vide. Assurez-vous d'être dans un dépôt Git valide.")
+        print(f"{Fore.LIGHTRED_EX}Le répertoire .git est vide. Assurez-vous d'être dans un dépôt Git valide.{Style.RESET_ALL}")
         return
     
     existing_hookity_config = os.path.join(current_directory, 'hookity')
     if os.path.isdir(existing_hookity_config):
-        print("Il semblerai qu'une configuration hookity soit déjà mise en place ...")
-        print("Lancer la commande 'hookity install', pour installer les configurations Hookity")
+        print(f"{Fore.LIGHTYELLOW_EX}Il semblerai qu'une configuration hookity soit déjà mise en place ...{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTWHITE_EX}Lancer la commande {Fore.LIGHTGREEN_EX}'hookity install'{Style.RESET_ALL}{Fore.LIGHTWHITE_EX}, pour installer les configurations Hookity{Style.RESET_ALL}")
         return
     
-    print("Nous devons savoir qui vous êtes !")
-    organization_name = get_user_input("Nom de votre projet ou de votre société : ")
+    print(f"{Fore.LIGHTMAGENTA_EX}{Style.BRIGHT}Nous devons savoir qui vous êtes :{Style.RESET_ALL}")
+
+    organization_name = get_user_input(f"{Style.BRIGHT}Nom de votre projet ou de votre société : {Style.RESET_ALL}")
     
-    print("Configuration des normes conventionnelles pour vos branches Git")
-    branch_type_autorized = get_user_input("Type de branches autorisés (Ex. Feat, Hotfix, Release, Docs, ... ) (Séparé par des espaces, si plusieurs): ")
+    print(f"{Fore.LIGHTMAGENTA_EX}{Style.BRIGHT}Configuration des normes conventionnelles pour vos branches Git :{Style.RESET_ALL}")
+
+    branch_type_autorized = get_user_input(f"{Style.BRIGHT}Type de branches autorisés (Ex. Feat,Hotfix,Release,Docs,... ) (Séparé par des ,) : {Style.RESET_ALL}")
     
-    print("Configuration des normes conventionnelles pour vos messages de commit Git")
-    commit_type_autorized = get_user_input("Type de commit autorisés (Ex. build|docs|bug|feat|fix|experiment|refactor|test... ) (Séparé par des espaces, si plusieurs): ")
-    commit_scope_authorized = get_user_input("Scope de commit autorisés (Ex. controllers|route|middleware|view|config|service|api... ) (Séparé par des espaces, si plusieurs) (Mettre * pour laisser libre): ")
+    print(f"{Fore.LIGHTMAGENTA_EX}{Style.BRIGHT}Configuration des normes conventionnelles pour vos messages de commit Git : {Style.RESET_ALL}")
+
+    commit_type_autorized = get_user_input(f'''{Style.BRIGHT}Type de commit autorisés 
+                                           (Ex. build,docs,bug,feat,fix,experiment,refactor,test... ) | 
+                                           (Séparé par des , ) : {Style.RESET_ALL}''')
+    
+    commit_scope_authorized = get_user_input(f'''{Style.BRIGHT}Scope de commit autorisés
+                                             (Ex. controllers,route,middleware,view,config,service,api... ) | 
+                                             (Séparé par des , ) {Fore.LIGHTRED_EX}(Laisse vide pour rien) : {Style.RESET_ALL}''')
     
 
     config_data = {
@@ -43,46 +57,98 @@ def init_command():
         'commit_scope_authorized': commit_scope_authorized.split(),
     }
 
-    hookity_resources = os.path.join(os.path.dirname(__file__), '..', 'resources', 'hookity')
-    shutil.copytree(hookity_resources, os.path.join(current_directory, 'hookity'), dirs_exist_ok=True)
+    hookity_resources = os.path.join(os.path.dirname(__file__), '..', 'resources', '.hookity')
+    shutil.copytree(hookity_resources, os.path.join(current_directory, '.hookity'), dirs_exist_ok=True)
 
-    config_path = os.path.join(current_directory, 'hookity', 'hookity_config.json')
+    config_path = os.path.join(current_directory, '.hookity', 'hookity_config.json')
     with open(config_path, 'w') as config_file:
         json.dump(config_data, config_file, indent=2)
 
-    print(f"Hookity initialisé avec succés !")
+    print(f"{Fore.GREEN}{Style.BRIGHT}Hookity initialisé avec succés dans votre dépôt Git, un nouveau dossier .hookity à été créer a la racine de votre projet git !{Style.RESET_ALL}")
+    print(f"{Fore.LIGHTYELLOW_EX}{Style.BRIGHT}Vous pouvez à présent push le dossier .hookity sur votre dépôt Git afin que vos collaborateurs puisse installer les configurations{Style.RESET_ALL}")
+    print(f"{Fore.LIGHTWHITE_EX}{Style.BRIGHT}Exécuter la commande {Fore.LIGHTGREEN_EX}{Style.BRIGHT}'hookity activate'{Style.RESET_ALL}{Fore.LIGHTWHITE_EX}{Style.BRIGHT} afin d'activer vos configurations et les rendre actives.'{Style.RESET_ALL}")
 
 
-def install_command():
-    hookity_directory = os.path.join(os.getcwd(), 'hookity')
-    setup_hooks_script = os.path.join(hookity_directory, 'setup_hooks.sh')
+def activate_hookity_command():
+    hookity_script_directory = os.path.join(os.getcwd(), '.hookity', 'scripts')
+    activate_hooks_script = os.path.join(hookity_script_directory, 'activate_hookity.sh')
 
-    if os.path.exists(setup_hooks_script):
-        subprocess.run(['bash', setup_hooks_script])
+    if os.path.exists(activate_hooks_script):
+        subprocess.run(['bash', activate_hooks_script])
     else:
-        print("Erreur : Le script setup_hooks.sh n'a pas été trouvé dans le dossier hookity.")
+        print(f"{Fore.LIGHTRED_EX}{Style.BRIGHT}Erreur : Le script activate_hookity.sh n'a pas été trouvé dans le dossier hookity.{Style.RESET_ALL}")
 
-# Faire deactivate command
 
-# Faire uninstall command
+def deactivate_hookity_command():
+    hookity_script_directory = os.path.join(os.getcwd(), '.hookity', 'scripts')
+    deactivate_hooks_script = os.path.join(hookity_script_directory, 'deactivate_hookity.sh')
+
+    if os.path.exists(deactivate_hooks_script):
+        subprocess.run(['bash', deactivate_hooks_script])
+    else:
+        print(f"{Fore.LIGHTRED_EX}{Style.BRIGHT}Erreur : Le script deactivate_hookity.sh n'a pas été trouvé dans le dossier hookity.{Style.RESET_ALL}")
+
+
+def uninstall_hookity_command():
+    while True:
+        user_choice = get_user_input(f"{Fore.LIGHTYELLOW_EX}{Style.BRIGHT}Attention, vous allez désinstaller complètement Hookity de votre dépôt git. Êtes-vous sûr ? {Fore.LIGHTBLUE_EX}(O ou N) : {Style.RESET_ALL}")
+
+        if user_choice.upper() == "O":
+            hookity_script_directory = os.path.join(os.getcwd(), '.hookity', 'scripts')
+            uninstall_hooks_script = os.path.join(hookity_script_directory, 'uninstall_hookity.sh')
+
+            if os.path.exists(uninstall_hooks_script):
+                subprocess.run(['bash', uninstall_hooks_script])
+                break
+            else:
+                print(f"{Fore.LIGHTRED_EX}{Style.BRIGHT}Erreur : Le script uninstall_hookity.sh n'a pas été trouvé dans le dossier hookity.{Style.RESET_ALL}")
+                break
+        elif user_choice.upper() == "N":
+            print(f"{Fore.LIGHTBLUE_EX}{Style.BRIGHT}Désinstallation annulée.{Style.RESET_ALL}")
+            break 
+        else:
+            print(f"{Fore.LIGHTRED_EX}{Style.BRIGHT}Erreur : Choix invalide. Veuillez entrer 'O' pour oui ou 'N' pour non.{Style.RESET_ALL}")
+
+def version_command():
+    try:
+        version = subprocess.check_output(['python3', 'setup.py', '--version'], text=True).strip()
+        print(f"{Fore.LIGHTCYAN_EX}{Style.BRIGHT}Hookity CLI {version}{Style.RESET_ALL}")
+    except subprocess.CalledProcessError as e:
+        print(f"{Fore.LIGHTRED_EX}{Style.BRIGHT}Erreur lors de la lecture de la version : {e}{Style.RESET_ALL}")
 
 def main():
-    parser = argparse.ArgumentParser(description='Hookity CLI v0.1')
+    parser = argparse.ArgumentParser(description='Hookity CLI')
+
+    # Commande version
+    parser.add_argument('--version', action='store_true', help="Affiche la version de Hookity")
 
     subparsers = parser.add_subparsers(title="Liste des commandes autorisées",help='Commandes disponibles', dest='command')
 
     # Init command
-    subparsers.add_parser('init', help='Initialisation de Hookity dans votre projet git.')
+    subparsers.add_parser('init', help='Initialisation et configuration de Hookity dans votre projet git.')
 
-    # Install command
-    subparsers.add_parser('activate', help='Installer et initialiser les configurations Hookity Git dans votre repository.')
+    # Activate command
+    subparsers.add_parser('activate', help='Désactiver les configurations Hookity Git dans votre repository (Local).')
+
+    # Deactivate command
+    subparsers.add_parser('deactivate', help='Désactiver les configurations Hookity Git dans votre repository (Local).')
+
+    # Uninstall command
+    subparsers.add_parser('uninstall', help='Désinstaller et désinitialiser toutes les configurations Hookity Git dans votre repository (HARD).')
+
 
     args = parser.parse_args()
 
     if args.command == 'init':
         init_command()
-    elif args.command == "activate":
-        install_command()
+    elif args.command == 'activate':
+        activate_hookity_command()
+    elif args.command == 'deactivate':
+        deactivate_hookity_command()
+    elif args.command == 'uninstall':
+        uninstall_hookity_command()
+    elif args.version:
+        version_command()
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ setup(
     author="Thibault Conte",
     author_email="thibault1.conte@epitech.eu",
     description="Improve branch and commit message authorization for collaborator",
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     package_data={'hookity': ['resources/*']},
     install_requires=[
         'jq',
+        'colorama',
+        'json',
+        'shutil',
+        'subprocess'
     ],
     entry_points={
         'console_scripts': [
